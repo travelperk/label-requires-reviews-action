a05737102ce659475a315ad2fcce750dca60600d
Raul Mozo <raul.mozo@perk.com>
2026-01-26 13:07:05 +0100

    types

--- src/types.ts
+++ src/types.ts
@@ -6,10 +6,11 @@ export interface Rule {
 }
 
 export type IssuesListLabelsOnIssueParams =
-  Endpoints['GET /repos/:owner/:repo/issues/:issue_number/labels']['parameters']
+  Endpoints['GET /repos/{owner}/{repo}/issues/{issue_number}/labels']['parameters']
 export type IssuesListLabelsOnIssueResponse =
-  Endpoints['GET /repos/:owner/:repo/issues/:issue_number/labels']['response']
+  Endpoints['GET /repos/{owner}/{repo}/issues/{issue_number}/labels']['response']
+
 export type PullsListReviewsParams =
-  Endpoints['GET /repos/:owner/:repo/pulls/:pull_number/reviews']['parameters']
+  Endpoints['GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews']['parameters']
 export type PullsListReviewsResponse =
-  Endpoints['GET /repos/:owner/:repo/pulls/:pull_number/reviews']['response']
+  Endpoints['GET /repos/{owner}/{repo}/pulls/{pull_number}/reviews']['response']
